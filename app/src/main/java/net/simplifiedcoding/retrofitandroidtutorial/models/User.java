ace0c396d8ea0a1abaf902e7c83b4d9995f0f6d9
ogpoggi <bastienpoggi@msn.com>
2018-09-03 22:37:34 +0200

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/retrofitandroidtutorial/models/User.java
@@ -0,0 +1,26 @@
+package net.simplifiedcoding.retrofitandroidtutorial.models;
+
+public class User {
+
+    private int id;
+    private String email, name;
+
+    public User(int id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
